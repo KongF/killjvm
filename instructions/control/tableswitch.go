b2729e8f9bfcca4465ce848c1b16d1d6c6ce9349
kongfanqin <kongfq@buyelec.net>
2023-05-19 13:35:55 +0800

    switch

--- instructions/control/tableswitch.go
+++ instructions/control/tableswitch.go
@@ -1,12 +1,15 @@
 package control
 
-import "killjvm/instructions/base"
+import (
+	"killjvm/instructions/base"
+	"killjvm/rtda"
+)
 
 type TABLE_SWITCH struct {
-	defaultOffset    int32
-	low                int32
-	high               int32
-	jumpOffsets      []int32
+	defaultOffset int32
+	low           int32
+	high          int32
+	jumpOffsets   []int32
 }
 
 func (self *TABLE_SWITCH) FetchOperands(reader *base.BytecodeReader) {
@@ -18,7 +21,26 @@ func (self *TABLE_SWITCH) FetchOperands(reader *base.BytecodeReader) {
 	self.jumpOffsets = reader.ReadInt32s(jumpOffsetsCount)
 }
 func (self *BytecodeReader) SkipPadding() {
-	for self.pc%4 ! = 0 {
+	for self.pc%4 != 0 {
 		self.ReadUint8()
 	}
+}
+func (self *BytecodeReader) ReadInt32s(n int32) []int32 {
+	ints := make([]int32, n)
+	for i := range ints {
+		ints[i] = self.ReadInt32()
+	}
+	return ints
+}
+func (self *TABLE_SWITCH) Execute(frame *rtda.Frame) {
+	index := frame.OperandStack().PopInt()
+
+	var offset int
+	if index >= self.low && index <= self.high {
+		offset = int(self.jumpOffsets[index-self.low])
+	} else {
+		offset = int(self.defaultOffset)
+	}
+
+	base.Branch(frame, offset)
 }
---
+++ instructions/extended/wide.go
@@ -0,0 +1,7 @@
+package extended
+
+import "killjvm/instructions/base"
+
+type WIDE struct {
+	modifiedInstruction base.Instruction
+}
